0b4da82e6714f36a7daf7e3e822fe27b47f6ede3
Bartocha Kamil <kamil.bartocha@nokia.com>
2021-05-16 15:15:51 +0200

    dir for hull-white project and data

--- Statistical_models/project_student_performence.R
+++ Statistical_models/project_student_performence.R
@@ -1,102 +1,78 @@
+do_anova <- function(result, variable, var_name, res_name="Final Grade"){
+  print("==================================================")
+  print("==================================================")
+  print("==================================================")
+  
+  print("VARIABLE NAME:")
+  print(var_name)
+  
+  print("SUMMARY:")
+  print(summary(variable))
+  
+  print("BOX PLOTS:")
+  plot(factor(variable), result, xlab=var_name, ylab=res_name)
+  try({
+    print("HISTOGRAM FOR")
+    print(var_name)
+    hist(variable, main=var_name)}, silent = TRUE)
+  
+  try({
+    print("BARPLOT FOR")
+    print(var_name)
+    barplot(prop.table(table(variable)), main=var_name)}, silent = TRUE)
+  
+  
+  print("Start ANOVA for:")
+  print(res_name)
+  print("explained by:")
+  print(var_name)
+  
+  anova <- aov(result~variable, data.frame(result, variable))
+  
+  print("SUMMARY OF ANOVA")
+  print(summary(anova))
+  
+  lr <-lm(result~variable, data.frame(result, variable))
+  
+  print("SUMMARY OF LINEAR MODEL")
+  print(summary(lr))
+  print("RESIDUAL PLOTS")
+  plot(lr$residuals, main=var_name)
+  print("Shapiro normality test")
+  print(shapiro.test(lr$residuals))
+}
+
+do_anova_for_each_column <- function(result, no_zero_res,
+                                     data, no_zero_data,
+                                     res_name="Final Grade",
+                                     no_zero_res_name="Final Grade with out 0"){
+  for (name in colnames(data)){
+    do_anova(result, factor(data[[name]]), name, res_name)
+    print("Now FOR ANALISYS FOR RESULTS WITH OUT 0")
+    do_anova(no_zero_res, factor(no_zero_data[[name]]), name, no_zero_res_name)
+    sys.sleep(180)
+  }
+}
+
+
+
 # Load data
 data = read.table("student-mat.csv",sep=";",header=TRUE)
-data_test <- data[0:100, ]
-data_train <- data[100:395, ]
-reg<-lm(data_train$G3~., data_train)
-summary(reg)
-plot(data_train$G3, reg$res)
-shapiro.test(reg$res)
-# cleaning data from rows containing G3 = 0
-data_train = data_train[data_train$G3 != 0,]
-# ToDo fix this below
-##### men = data[data$sex != "F"]
-##### men = mapvalues(data, data$sex)
-##### women = data[data$sex != "M"]
-# print(nrow(data)) # 382 students
-
-#summary(data)
-#boxplot(data)
-
-reg<-lm(data_train$G3~., data_train)
-summary(reg)
-plot(data_train$G3, reg$res)
-# we can see that famrel, absences, G1, G2, paid, health are essential
-# Lets check residuals
-# We dont see any dependence
-shapiro.test(reg$res)
-data_train_tmp = data_train[data_train$G3 != 0,]
-# so we accept/reject hypothesis about normality of residuals
-# Lest see qq plots of residuals 
-qqnorm(reg$res)
-
-# New model
-new_reg <-lm(data_train$G3~data_train$famrel+data_train$absences+data_train$G1+data_train$G2+data_train$paid+data_train$health, data_train)
-summary(new_reg)
-
-# Lets check residuals
-plot(data_train$G3, new_reg$res)
-# We dont see any dependence
-shapiro.test(new_reg$res)
-# so we accept/reject hypothesis about normality of residuals
-# Lest see qq plots of residuals 
-qqnorm(new_reg$res)
-
-# Lest check dependencies residuals of variables
-plot(data$G3 ,new_reg$res)
-abline(h=0)
-plot(data$famrel ,new_reg$res)
-abline(h=0)
-plot(data$health ,new_reg$res)
-abline(h=0)
-plot(data$absences ,new_reg$res)
-abline(h=0)
-plot(data$G1 ,new_reg$res)
-abline(h=0)
-plot(data$G2 ,new_reg$res)
-abline(h=0)
-# We dont see any dependencies
-
-# Lets check a smaller model with out variables G1 and G2
-new_reg2 <-lm(data$G3~data$famrel+data$absences+data$paid+data$health, data)
-summary(new_reg2)
-# We see that, it is a bad idea
-
-
-shapiro.test(reg$res)
-shapiro.test(new_reg$residuals)
-plot(data$G3, new_reg$residuals)
-new_results = data$G3
-
-# Lets check the model with only G1 and G2
-new_reg_abs <-lm(data$G3~data$G1+data$G2, data)
-summary(new_reg_abs)
-# We can see that it is the best model, we can see that G2 is more essential variables that G1
-
-# Check residuals 
-plot(data$G3, new_reg_abs$res)
-shapiro.test(new_reg_abs$res)
-qqnorm(new_reg_abs$res)
-# looks good
-
-# Lets check the model with only G2
-new_reg_g2 <-lm(data$G3~data$G2, data)
-summary(new_reg_g2)
-# ToDo write the results
+no_zero_data <- data[data$G3 != 0,]
+fg <- data$G3
+no_zero_fg <- no_zero_data$G3
+print("SAMMARY OF FINAL GRADE")
+summary(fg)
+hist(fg, main = "HISTOGRAM FOR FINAL GRADE")
+no_res = subset(data, select = -c(G1, G2, G3))
+no_zero_res = subset(no_zero_data, select = -c(G1, G2, G3))
+do_anova_for_each_column(fg, no_zero_fg, no_res, no_zero_res)
 
 
 
 
-# ***********************
-#------   ANOVA   --------
-# ***********************
 
-# We will research if the sex has influence on the G3
-# prepare data
-grades = data$G1
-sex = data$sex
-# build ANOVA
-anova.tool <- aov(grades~sex,data)
-print(summary(anova.tool))
-# we can see that sex has/hasn't a influence on the G3
 
-# ToDo makes some plots
+
+
+# TYLA CI WYSTARCZY :)
